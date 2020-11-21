0e498a3ea3e810c9b9db4ab22539d6b7ed3054e8
Attila Soltész <60141838+solattila@users.noreply.github.com>
2020-11-21 20:12:24 +0100

    Add entity framework

---
+++ ShopApp/ShopApp.DataAcces.SQL/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace ShopApp.DataAcces.SQL.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ShopApp.DataAcces.SQL.DataContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(ShopApp.DataAcces.SQL.DataContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
